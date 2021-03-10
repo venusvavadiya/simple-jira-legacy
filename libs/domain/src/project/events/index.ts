53aed1dead213c193268e3e8107d30aa7d45e4c8
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-09 23:12:56 -0600

    Deleted

--- libs/domain/src/project/events/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './project-created-v1.event';
--- libs/domain/src/project/events/project-created-v1.event.ts
+++
@@ -1,7 +0,0 @@
-import { Event } from '../../lib/event';
-
-export class ProjectCreatedV1 extends Event {
-  constructor(public readonly projectId: string) {
-    super();
-  }
-}
