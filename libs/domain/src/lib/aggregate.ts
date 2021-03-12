d4dfe9bb7075e82b0604368e2f0fd7ea2e756df8
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-11 19:31:16 -0600

    Scoped and Repository added

--- libs/domain/src/lib/aggregate.ts
+++ libs/domain/src/lib/aggregate.ts
@@ -1,24 +1,28 @@
 import { Event } from './event';
 
-export abstract class Aggregate {
+export class Aggregate {
   id: string;
   changes: Event[];
   version = 0;
 
-  applyEvent(event: Event, isCommited = false) {
-    const eventName = event.constructor.name;
-    const methodName = `apply${eventName}`;
-    this[methodName](event);
-    if (!isCommited) this.changes.push(event);
+  static createInstance<T extends Aggregate>(): T {
+    return new this() as T;
   }
 
-  loadFromEvents(events: Event[]) {
+  loadFromEvents(events: Event[]): void {
     events.forEach((event) => {
       this.applyEvent(event, true);
     });
   }
 
-  markAsCommited() {
+  markAsCommited(): void {
     this.changes = [];
+  }
+
+  protected applyEvent(event: Event, isCommited = false): void {
+    const eventName = event.constructor.name;
+    const methodName = `apply${eventName}`;
+    this[methodName](event);
+    if (!isCommited) this.changes.push(event);
   }
 }
