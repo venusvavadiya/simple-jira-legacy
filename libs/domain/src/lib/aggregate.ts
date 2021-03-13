d058c1183a1d030cc1c2f37be9d54353fac2f851
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-12 18:53:44 -0600

    Reset changes

--- libs/domain/src/lib/aggregate.ts
+++ libs/domain/src/lib/aggregate.ts
@@ -3,22 +3,25 @@ import { Event } from './event';
 export class Aggregate {
   id: string;
   changes: Event[];
-  version = 0;
+  version = -1;
 
   loadFromEvents(events: Event[]): void {
-    events.forEach((event) => {
-      this.applyEvent(event, true);
-    });
+    events.forEach(this.applyEvent);
   }
 
-  markAsCommited(): void {
+  resetChanges(): void {
     this.changes = [];
   }
 
-  protected applyEvent(event: Event, isCommited = false): void {
+  protected raiseEvent(event: Event): void {
+    this.applyEvent(event);
+    this.changes.push(event);
+  }
+
+  private applyEvent(event: Event): void {
     const eventName = event.constructor.name;
     const methodName = `apply${eventName}`;
     this[methodName](event);
-    if (!isCommited) this.changes.push(event);
+    this.version += 1;
   }
 }
