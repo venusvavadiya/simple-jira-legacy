a596e2f6681b7e6e47ab14bb9da5d560c5f5717d
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-21 14:44:22 -0500

    EventStoreDB

--- apps/graphql/src/app/event-store-db.event-store.ts
+++ apps/graphql/src/app/event-store-db.event-store.ts
@@ -1,9 +1,11 @@
 import {
-  EventStoreDBClient, FORWARDS,
-  jsonEvent, NO_STREAM, START,
+  EventStoreDBClient,
+  FORWARDS,
+  jsonEvent,
+  JSONType,
+  START,
 } from '@eventstore/db-client';
 import { Event, EventStore } from '@simple-jira/domain';
-import { v4 as uuidv4 } from 'uuid';
 
 export class EventStoreDbEventStore implements EventStore {
   client
@@ -12,40 +14,16 @@ export class EventStoreDbEventStore implements EventStore {
     this.client = EventStoreDBClient.connectionString('esdb://localhost:2113?tls=false');
   }
 
-  // eslint-disable-next-line no-unused-vars
   async append(stream: string, events: Event[], version: number): Promise<void> {
-    // const mappedEvents = events.map((x) => jsonEvent({ type: 'created', data: { ...x } }));
-    // await this.client.appendToStream(stream, mappedEvents, { expectedRevision: version });
-
-    const eventOne = jsonEvent({
-      id: uuidv4(),
-      type: 'some-evenat',
-      data: {
-        id: '1',
-        value: 'some value',
-      },
-    });
-
-    // const eventTwo = jsonEvent({
-    //   id: uuidv4(),
-    //   type: 'some-event',
-    //   data: {
-    //     id: '2',
-    //     value: 'some other value',
-    //   },
-    // });
-
-    await this.client.appendToStream('as-stream', eventOne).then(console.log);
+    const mapped = events
+      .map((x) => jsonEvent({ type: x.constructor.name, data: x as unknown as JSONType }));
+    await this.client.appendToStream(stream, mapped, { expectedRevision: version });
   }
 
-  async read(stream) {
-    const events = await this.client.readStream(stream, {
+  read(stream) {
+    return this.client.readStream(stream, {
       direction: FORWARDS,
       fromRevision: START,
     });
-
-    events.forEach(console.log);
-
-    return Promise.resolve([]);
   }
 }
